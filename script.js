626072476a957a99b8de5e27cd12f28bacb222bb
LeclercMoonsilver <leclercmoonsilver@gmail.com>
2026-08-27 10:27:34 +0000

    committed changes adding effect for images

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function Insert(PreviewImage) {
-    document.getElementById("ImageView").style.backgroundImage = "url('" + PreviewImage.src + '")";  
+    document.getElementById('ImageView').style.backgroundImage = "url('" + PreviewImage.src + "')";
+    document.getElementById('ImageView').innerHTML = "";
+    document.getElementById('desc').innerHTML = PreviewImage.alt;
 }
 
 function Undo() {
-
+    document.getElementById('ImageView').style.backgroundImage = "none";
+    document.getElementById('ImageView').innerHTML = "Hover over an image to view a cat breed"
+    document.getElementById('desc').innerHTML = "Hover an image to view description"
 }
